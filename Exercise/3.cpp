ab2ab1b5d0a7d3bf96dcd5ed333775d50c84fc7a
Atanu Majumder <atanumajumder2004@gmail.com>
2023-10-03 15:54:42 +0530

    added function method

--- Exercise/3.cpp
+++ Exercise/3.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+/*
 int main(){
 
     int p;
@@ -20,6 +21,32 @@ int main(){
 
     cout<<"Simple interest is "<<simpleInterest<<endl;
 
+    return 0;
+}
+
+*/
+
+// or by using function
+
+int calculate(int p, int r, int t)
+{
+    return (p * r * t) / 100;
+}
+
+int main()
+{
+
+    int p, r, t;
+
+    cout << "Enter the principal amount: ";
+    cin >> p;
+    cout << "Enter the rate of interest: ";
+    cin >> r;
+    cout << "Enter the time: ";
+    cin >> t;
+
+    int simpleInterest = calculate(p, r, t);
+    cout << "Simple Interest = " << simpleInterest << endl;
 
     return 0;
 }
